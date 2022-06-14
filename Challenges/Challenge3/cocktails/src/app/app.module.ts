20895f3c1bd5c7dafa86dc154730592b05295984
Antonio Moschillo <argomoschillo@gmail.com>
2022-06-14 12:22:31 +0200

    ng-template added

--- Challenges/Challenge3/cocktails/src/app/app.module.ts
+++ Challenges/Challenge3/cocktails/src/app/app.module.ts
@@ -2,11 +2,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomeModule } from './home/home.module';
 import { HttpClientModule } from '@angular/common/http';
+
+import { FavModule } from './fav/fav.module';
 
 
 
@@ -21,7 +22,8 @@ import { HttpClientModule } from '@angular/common/http';
     HomeModule,
     SharedModule,
     HomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FavModule
   ],
   providers: [],
   bootstrap: [AppComponent]

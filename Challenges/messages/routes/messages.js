c7fe77f6702d1cb6e20207d18d62291ef26ba0cb
Albrim Sulaj <asulaj99@gmail.com>
2022-06-16 23:37:29 +0200

    fineapi

--- Challenges/messages/routes/messages.js
+++ Challenges/messages/routes/messages.js
@@ -3,29 +3,70 @@ import cors from 'cors';
 
 const router = express.Router();
 router.use(cors());
-const messagesDuringMatch = [
-    {
-        comment :"Che mossa era?!" 
-    },
-     {
-        comment :"Stai facendo sul serio?!" 
+
+const messagesLoss = [{
+        comment: 'Pensavi eeeh'
     },
     {
-        comment :"Sei tu che hai scorreggiato?!" 
+        comment: 'Magari se gioco bendato vinci'
     },
     {
-        comment :"Ti spiezzo in due!!" 
+        comment: 'Ieri sera tua madre ha giocato meglio!'
     },
-      {
-        comment :"Per me è no!"
+    {
+        comment: 'Dai la prossima te la lascio'
+    },
+    {
+        comment: 'Ero pronto a dire fortuna del principiante ma sei troppo scarso'
+    }
+];
+const messagesDraw = [{
+        comment: 'Non male'
+    },
+    {
+        comment: 'Brava Bernharde'
+    },
+    {
+        comment: 'Se non ti lascio un po di soddisfazione ti arrendi sicuro...'
+    }, {
+        comment: 'Stavo mangiando...'
+    }, {
+        comment: 'Mi facevo la doccia...'
+    }, {
+        comment: 'Bravo Fanyi'
+    }, {
+        comment: 'Brava Aldessantro '
+    }
+]
+const messagesDuringMatch = [{
+        comment: "Che mossa era?!"
+    },
+    {
+        comment: "Stai facendo sul serio?!"
+    },
+    {
+        comment: "Sei tu che hai scorreggiato?!"
+    },
+    {
+        comment: "Ti spiezzo in due!!"
+    },
+    {
+        comment: "Per me è no!"
     },
     {
         comment: "Oddio sto tremando!!"
-    }  
+    }
 ];
 
-router.get('/messagesdm',(req, res) => {   // messages during the match!
+router.get('/messagesduringmatch', (req, res) => { // messages during the match!
     res.send(messagesDuringMatch)
 });
+
+router.get('/messagesdraw', (req, res) => {
+    res.send(messagesDraw);
+})
+router.get('/messageslost', (req, res) => {
+    res.send(messagesLoss);
+})
 
 export default router;

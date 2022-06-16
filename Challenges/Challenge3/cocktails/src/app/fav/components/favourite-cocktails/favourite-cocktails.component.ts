e05c30b846a90b88e05c576307292bf9a0144f1d
Antonio Moschillo <argomoschillo@gmail.com>
2022-06-16 12:11:43 +0200

    page-favourite started

--- Challenges/Challenge3/cocktails/src/app/fav/components/favourite-cocktails/favourite-cocktails.component.ts
+++ Challenges/Challenge3/cocktails/src/app/fav/components/favourite-cocktails/favourite-cocktails.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CocktailsService } from 'src/app/services/cocktails.service';
+import { ICocktail } from '../../../models/cocktails';
+import { map } from 'rxjs'
 
 @Component({
   selector: 'app-favourite-cocktails',
@@ -7,13 +9,39 @@ import { CocktailsService } from 'src/app/services/cocktails.service';
   styleUrls: ['./favourite-cocktails.component.css']
 })
 export class FavouriteCocktailsComponent implements OnInit {
+  myFavoriteCocktails: ICocktail[] = [];
+  arrayN: Array<number> = [];
 
-  constructor(private cocktailService: CocktailsService) { }
+  constructor(private cocktailService: CocktailsService) {
+ 
+   }
 
   ngOnInit(): void {
-    this.cocktailService.getFavouriteCocktails().subscribe(
-      (data: any) => { console.log(data) }
-    )
-  }
+   this.cocktailService.getFavouriteCocktails().pipe(
+    map((response: any) => {
+      for (let x of response){
+        this.myFavoriteCocktails.push(x);        
+      }      
+    })
+   )
+   .subscribe()
+    console.log(this.myFavoriteCocktails);
+
+    this.cocktailService.getFavouriteCocktails().pipe(
+      map((response: any)=> {
+       for(let x of response){
+        this.arrayN.push(x.totStars)
+       }
+      })
+    ).subscribe()
+    console.log(this.arrayN)
+  
+  };
+
+  
+
+  
+
+  
 
 }
--- Challenges/Challenge3/cocktails/src/app/models/cocktails.ts
+++ Challenges/Challenge3/cocktails/src/app/models/cocktails.ts
@@ -5,7 +5,9 @@ export interface ICocktail{
     strIBA: string;
     strCategory: string;
     strDrinkThumb: string;
+    strInstructions: string;
     strInstructionsEn: string;
+    strInstructionsIt: string;
     stars?: number;
     strIngredient1?: string;
     strIngredient2?: string;
@@ -22,4 +24,5 @@ export interface ICocktail{
     strIngredient13?: string;
     strIngredient14?: string;
     strIngredient15?: string;
+    totStars: number
 }
